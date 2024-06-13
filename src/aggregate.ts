44a338af28215cf1ddfbde01deb3f937ffa668ec
Erik Engervall <erik.engervall@gmail.com>
2024-06-13 18:53:46 +0900

    🚀 ~ src/aggregate.ts

--- src/aggregate.ts
+++ src/aggregate.ts
@@ -24,28 +24,41 @@ export async function aggregate(options: CloudWatchLogsParserOptions) {
     const lines = logStreamFileContent.split('\n');
     for (const line of lines) {
       // 2012-12-12T12:12:12.123Z {"level":"info","message":"Processing something","timestamp":"2012-12-12 12:12:12"}
-      const subline = line.substring('2012-12-12T12:12:12.123Z '.length); // remove timestamp
+      // const datelessLine = line.substring('2012-12-12T12:12:12.123Z '.length); // remove timestamp
+      const datelessLine = line.replace(
+        /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/,
+        '',
+      );
 
       /**
        * @example {"level":"info","message":"Processing something","timestamp":"2012-12-12 12:12:12"}
        *          ^^^^^^^^^
        */
-      if (subline.startsWith('{"level":')) {
-        const { timestamp, message, ...rest } = (jsonParseSafe(subline) ??
+      if (datelessLine.startsWith('{"level":')) {
+        const { timestamp, message, ...rest } = (jsonParseSafe(datelessLine) ??
           {}) as Record<string, unknown>;
         timestamp; // omit timestamp
-        if (typeof message === 'string') {
-          if (!map[message]) {
-            map[message] = {
-              count: 0,
-              payloads: {},
-            };
-          }
-          map[message].count += 1;
-          const stringifiedRest = JSON.stringify(rest);
-          map[message].payloads[stringifiedRest] =
-            (map[message].payloads[stringifiedRest] ?? 0) + 1;
+        if (typeof message !== 'string') {
+          logger.warn('Message is not a string', {
+            datelessLine,
+            message,
+            messageType: typeof message,
+          });
+          continue;
         }
+
+        // initialize
+        if (!map[message]) {
+          map[message] = {
+            count: 0,
+            payloads: {},
+          };
+        }
+
+        map[message].count += 1;
+        const stringifiedRest = JSON.stringify(rest);
+        map[message].payloads[stringifiedRest] =
+          (map[message].payloads[stringifiedRest] ?? 0) + 1;
         continue;
       }
 
@@ -54,18 +67,21 @@ export async function aggregate(options: CloudWatchLogsParserOptions) {
        *           ^^^
        */
       if (
-        subline.startsWith('err') ||
-        subline.startsWith('war') ||
-        subline.startsWith('inf') ||
-        subline.startsWith('deb')
+        datelessLine.startsWith('err') ||
+        datelessLine.startsWith('war') ||
+        datelessLine.startsWith('inf') ||
+        datelessLine.startsWith('deb')
       ) {
-        const [message, payload] = subline.split('{');
+        const [message, payload] = datelessLine.split('{');
+
+        // initialize
         if (!map[message]) {
           map[message] = {
             count: 0,
             payloads: {},
           };
         }
+
         map[message].count += 1;
         map[message].payloads[payload] =
           (map[message].payloads[payload] ?? 0) + 1;
